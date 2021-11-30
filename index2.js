bde3a4c436305a63415ac0eadd770f2b123d4a34
DavidBaekeland <71763779+DavidBaekeland@users.noreply.github.com>
2021-11-30 12:32:18 +0100

    index2.js: credentials

--- index2.js
+++ index2.js
@@ -1,43 +1,53 @@
+const express = require('express');
+const bodyParser = require('body-parser')
 const { MongoClient } = require("mongodb");
- 
-// Replace the following with your Atlas connection string                                                                                                                                        
-const url = "mongodb+srv://admin:admin@cluster0.wdkwv.mongodb.net/Session7_FullStackTeamwork?retryWrites=true&w=majority";
-const client = new MongoClient(url);
- 
- // The database to use
- const dbName = "Session7_FullStackTeamwork";
-                      
- async function run() {
+require('dotenv').config();
+
+// create the mongo Client to use
+const client = new MongoClient(process.env.FINAL_URL2)
+
+const app = express();
+const port = process.env.PORT;
+
+
+app.use(express.static("public"));
+// alle code wordt eerst uitgevoerd door middelware (bodyParser) dan in functies
+app.use(bodyParser.json());
+
+
+app.get('/', async(req, res) => {
     try {
-         await client.connect();
-         console.log("Connected correctly to server");
-         const db = client.db(dbName);
+        await client.connect();
+        console.log("Connected correctly to server");
+        const db = client.db(process.env.DB);
 
-         // Use the collection "people"
-         const col = db.collection("challenges");
+        // Use the collection "people"
+        const col = db.collection("challenges");
 
-         // Construct a document                                                                                                                                                              
-         let challengeDocument = {
-             name: "Challenge2",
-             points: 5,                                                                                                                             
-             course: "web2",                                                                                                                                
-             session: "s7a",
-         }
+        // Construct a document                                                                                                                                                              
+        let challengeDocument = {
+            name: "Challenge2",
+            points: 5,                                                                                                                             
+            course: "web2",                                                                                                                                
+            session: "s7a",
+        }
 
-         // Insert a single document, wait for promise so we can read it back
-         const p = await col.insertOne(challengeDocument);
-         // Find one document
-         const myDoc = await col.findOne();
-         // Print to the console
-         console.log(myDoc);
+        // Insert a single document, wait for promise so we can read it back
+        const p = await col.insertOne(challengeDocument);
+        // Find one document
+        const myDoc = await col.findOne();
+        // Print to the console
+        res.send(myDoc);
 
-        } catch (err) {
-         console.log(err.stack);
-     }
- 
-     finally {
-        await client.close();
+       } catch (err) {
+        console.log(err.stack);
     }
-}
 
-run().catch(console.dir);
+    finally {
+       await client.close();
+   }
+});
+
+app.listen(port, () => {
+    console.log(`My first REST API Example app listening at http://localhost:${port}`)
+  })
