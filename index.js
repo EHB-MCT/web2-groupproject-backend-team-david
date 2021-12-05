794998c08b18158d8efcd0c0c0fc7f19aafef706
DavidBaekeland <71763779+DavidBaekeland@users.noreply.github.com>
2021-12-05 15:41:06 +0100

    js: object

--- index.js
+++ index.js
@@ -39,8 +39,12 @@ app.get('/api/challenges', async(req, res) => {
 
         const DEVIII_collection = client.db('session5').collection('DEVIII');
         const challenge_DEVIII = await DEVIII_collection.find({}).toArray();
-        
-        res.status(200).send(challenge_DEVIII, challenge_WEBII);
+
+        let test = {
+            "DEV": challenge_DEVIII,
+            "WEB": challenge_WEBII
+        }
+        res.status(200).send(test);
     }catch(error)  {
         console.log(error);
         res.status(500).send({
